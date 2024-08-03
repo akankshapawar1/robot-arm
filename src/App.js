c3d2a8fa894940c5c0e1e1285b022bd4537016bc
Akanksha <42142815+akankshapawar1@users.noreply.github.com>
2024-08-03 18:48:23 -0400

    loaded the model on the screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Model } from "./Bazo_3D";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas style={{position:'absolute'}}>
+        <ambientLight intensity={5} />
+        <OrbitControls enableZoom={true} />
+        <Model />
+      </Canvas>
     </div>
   );
 }
